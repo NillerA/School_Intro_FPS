a370d5f57f186b9666fc9cdc2a3c22bf75c417f9
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2024-04-03 01:50:26 +0200

    Blueprints og Base_Klasser

---
+++ Source/School_Intro_FPS/School_Intro_FPS.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "School_Intro_FPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, School_Intro_FPS, "School_Intro_FPS" );
